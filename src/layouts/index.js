7cfe90bd07c65eadf91ddf9aabf8cd644fae52cf
Jason Brown <browniefed@gmail.com>
2018-03-04 20:47:29 -0800

    Add in new title

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,7 +7,7 @@ import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Helmet title="Every Day Diner" />
     <Navbar />
     <div>{children()}</div>
   </div>
